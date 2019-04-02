ed2e4984dc21daece5b5097e590de029d8dcebd4
Mika <mikavandenbrink@outlook.com>
2019-04-02 11:39:11 +0200

    Q1 works now!
    holy shiiiiit

--- FormuleCirkelEntity/Controllers/RacesController.cs
+++ FormuleCirkelEntity/Controllers/RacesController.cs
@@ -65,15 +65,7 @@ namespace FormuleCirkelEntity.Controllers
 
         public IActionResult Q1()
         {
-            return View(_context.SeasonDrivers.Include(s => s.Drivers).Include(t => t.SeasonTeam).ThenInclude(t => t.Team)
-                .Include(s => s.DriverResults).ToList());
-        }
-
-        [HttpPost]
-        public IActionResult Q1(SeasonDriver seasonDriver)
-        {
-            return View(_context.SeasonDrivers.Include(s => s.Drivers).Include(t => t.SeasonTeam).ThenInclude(t => t.Team)
-                .ToList());
+            return View();
         }
 
         public IActionResult Q2()
@@ -95,6 +87,53 @@ namespace FormuleCirkelEntity.Controllers
             result += driver.SeasonTeam.Chassis;
             result += rng.Next(0, 60);
             return result;
+        }
+
+        [HttpGet]
+        public JsonResult GetRacingDrivers()
+        {
+            int result = 0;
+            int position = 1;
+            var qualy = new List<Qualification>();
+            //var drivers = _context.DriverResults.Where(r => r.RaceId == raceid).Include(r => r.SeasonDriver).ToList();
+            
+            //To fill result of DriverResult it needs to have the SeasonDriverId and the RaceId
+            var drivers = _context.SeasonDrivers.Include(s => s.Drivers).Include(t => t.SeasonTeam).ThenInclude(t => t.Team)
+                .Include(d => d.SeasonDriverId).Select(d => new
+                {
+                    id = d.SeasonDriverId,
+                    team = d.SeasonTeam.Team.Name,
+                    name = d.Drivers.Name,
+                    skill = d.Skill,
+                    chassis = d.SeasonTeam.Chassis
+                }).ToList();
+
+            foreach(var driver in drivers)
+            {
+                result = driver.skill;
+                result += driver.chassis;
+                result += rng.Next(0, 60);
+                qualy.Add(new Qualification()
+                {
+                    Id = driver.id,
+                    TeamName = driver.team,
+                    DriverName = driver.name,
+                    Score = result
+                });
+            }
+            var ordered = qualy.OrderByDescending(q => q.Score);
+            foreach(var driver in ordered)
+            {
+                driver.Position = position;
+                position++;
+            }
+            return Json(ordered);
+        }
+
+        [HttpGet]
+        public JsonResult Test()
+        {
+            return Json("Piemel");
         }
 
         public IActionResult Race()
---
+++ FormuleCirkelEntity/Models/Qualification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormuleCirkelEntity.Models
+{
+    public class Qualification
+    {
+        public int? Position { get; set; }
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public string DriverName { get; set; }
+        public int Score { get; set; }
+    }
+}
