c9a4ed0273de7fef642b58da91f0a58a664bb1db
heerhaan <mikavandenbrink@outlook.com>
2019-04-25 21:44:08 +0200

    Oh, also Track
    Which means that every item in the indexes has validators now, could be expanded though.

---
+++ FormuleCirkelEntity/Models/ModelValidation/TrackValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using FormuleCirkelEntity.DAL;
+
+namespace FormuleCirkelEntity.Models.ModelValidation
+{
+    public class TrackValidator : AbstractValidator<Track>
+    {
+        public TrackValidator()
+        {
+            RuleFor(t => t.Name).NotEmpty().WithMessage("Naam circuit is verplicht!");
+            RuleFor(t => t.Location).NotEmpty().WithMessage("Locatie voor circuit is verplicht!");
+        }
+    }
+}
