47ee7c5af6bfa665db1b9ff3ba38c6882a62e76c
Rik Pronk <rik.pronk@outlook.com>
2020-02-05 23:26:20 +0100

    Add required dependencies and infrastructure types

---
+++ FormuleCirkeltrek.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace FormuleCirkeltrek.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        const string Mapping_Method = "Mapping";
+
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                    i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod(Mapping_Method);
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
