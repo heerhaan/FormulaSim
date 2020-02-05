9bb5ef9608e13c0a3216672f36da3e27928b5a8a
Rik Pronk <rik.pronk@outlook.com>
2020-02-05 23:27:33 +0100

    Add Application Test project with access to data layer

---
+++ FormuleCirkeltrek.Application.Tests/Common/DbContextFactory.cs
@@ -0,0 +1,63 @@
+﻿using FormuleCirkeltrek.Persistence;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FormuleCirkeltrek.Application.Tests.Common
+{
+    public class DbContextFactory : IDisposable
+    {
+        readonly SqliteConnection _connection;
+        readonly DbContextOptions<FormuleCirkeltrekDbContext> _options;
+
+        public DbContextFactory()
+        {
+            // In-memory database only exists while the connection is open
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            _options = new DbContextOptionsBuilder<FormuleCirkeltrekDbContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+
+            // Create the schema in the database
+            using (var context = new FormuleCirkeltrekDbContext(_options))
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public FormuleCirkeltrekDbContext Create()
+        {
+            return new FormuleCirkeltrekDbContext(_options);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Create the schema in the database
+                    using (var context = new FormuleCirkeltrekDbContext(_options))
+                    {
+                        context.Database.EnsureDeleted();
+                    }
+
+                    _connection.Close();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ FormuleCirkeltrek.Application.Tests/Common/QueryTestContext.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using FormuleCirkeltrek.Application.Common.Interfaces;
+using FormuleCirkeltrek.Application.Common.Mappings;
+using FormuleCirkeltrek.Persistence;
+using System;
+using System.Linq;
+
+namespace FormuleCirkeltrek.Application.Tests.Common
+{
+    public class QueryTestContext : IDisposable
+    {
+        readonly  DbContextFactory _contextFactory;
+        readonly IMapper _mapper;
+
+        public QueryTestContext()
+        {
+            var configurationProvider = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+            _mapper = configurationProvider.CreateMapper();
+            _contextFactory = new DbContextFactory();
+        }
+
+        public FormuleCirkeltrekDbContext GetNewDataLayer()
+            => _contextFactory.Create();
+
+        public IMapper GetMapper()
+            => _mapper;
+
+        public void Dispose()
+        {
+            _contextFactory.Dispose();
+        }
+    }
+}
