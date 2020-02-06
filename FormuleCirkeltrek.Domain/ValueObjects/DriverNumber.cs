e6bf154ea32c9b29a79dc7b42e42e4e8a905613a
Rik Pronk <rik.pronk@outlook.com>
2020-02-06 21:37:20 +0100

    Update DriverNumber struct with more conversions

--- FormuleCirkeltrek.Domain/ValueObjects/DriverNumber.cs
+++ FormuleCirkeltrek.Domain/ValueObjects/DriverNumber.cs
@@ -1,36 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FormuleCirkeltrek.Domain.ValueObjects
 {
-    public struct DriverNumber
+    public struct DriverNumber : IEquatable<DriverNumber>, IEquatable<DriverNumber?>
     {
+        public DriverNumber(long number)
+            : this(number.ToString()) { }
+
+        public DriverNumber(int number)
+            : this(number.ToString()) { }
+
         public DriverNumber(short number)
-            : this(number.ToString())
-        {}
+            : this(number.ToString()) { }
 
         public DriverNumber(string number)
         {
-            _number = string.Empty;
-            TryAssign(this, number);
-        }
-
-        string _number;
-
-        static void TryAssign(DriverNumber instance, string number)
-        {
             if (number.Any(c => !char.IsDigit(c))
-                || number.Length > 2
-                || number.Length <= 0)
+                   || number.Length > 2
+                   || number.Length <= 0)
                 throw new FormatException("DriverNumber can only consist of 1 or 2 digits.");
 
             if (number == "0" || number == "00")
                 throw new FormatException("DriverNumber cannot be 0.");
-            instance._number = number;
+            _number = number;
         }
+
+        readonly string _number;
+
+        #region Overrides / Implementations
+
+        public override string ToString()
+            => _number.ToString();
+
+        public override bool Equals(object? obj)
+            => Equals(obj as DriverNumber?);
+
+        public override int GetHashCode()
+            => _number.GetHashCode();
+
+        public bool Equals(DriverNumber other)
+            => _number.Equals(other._number);
+
+        public bool Equals(DriverNumber? other)
+            => _number.Equals(other?._number);
+
+        #endregion
+
+        #region Implicit conversions
+
+        public static implicit operator DriverNumber(string value)
+            => new DriverNumber(value);
+
+        public static implicit operator DriverNumber(short value)
+            => new DriverNumber(value);
+
+        public static implicit operator DriverNumber(int value)
+            => new DriverNumber(value);
+
+        public static implicit operator DriverNumber(long value)
+            => new DriverNumber(value);
+
+        #endregion
     }
 }
