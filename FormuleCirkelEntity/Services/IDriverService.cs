1c9133467ef612829f3aa407a7235e5c28b9d3e2
heerhaan <mikavandenbrink@outlook.com>
2020-11-24 15:28:50 +0100

    Initial commit servicelayer
    Only have added the basics of the driverlayer as of now

---
+++ FormuleCirkelEntity/Services/IDriverService.cs
@@ -0,0 +1,53 @@
+﻿using FormuleCirkelEntity.DAL;
+using FormuleCirkelEntity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormuleCirkelEntity.Services
+{
+    public interface IDriverService
+    {
+        Task<Driver> GetDriverByIdAsync(int id);
+        IQueryable<Driver> GetDrivers();
+        Task<bool> AddDriverAsync(Driver driver);
+        Task<bool> ModifyDriverAsync(Driver driver);
+        Task<bool> ArchiveDriverAsync(int id);
+    }
+
+    public class DriverService : IDriverService
+    {
+        private readonly FormulaContext _context;
+
+        public DriverService(FormulaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Driver> GetDriverByIdAsync(int id)
+        {
+            return await _context.Drivers.FindAsync(id);
+        }
+
+        public IQueryable<Driver> GetDrivers()
+        {
+            return _context.Drivers;
+        }
+
+        public async Task<bool> AddDriverAsync(Driver driver)
+        {
+            return false;
+        }
+
+        public async Task<bool> ModifyDriverAsync(Driver driver)
+        {
+            return false;
+        }
+
+        public async Task<bool> ArchiveDriverAsync(int id)
+        {
+            return false;
+        }
+    }
+}
