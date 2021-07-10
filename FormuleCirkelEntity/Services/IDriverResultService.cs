fc67ecf4ad013087e949e5874419a8b10b9c44d5
Mika <mikavandenbrink@outlook.com>
2021-07-10 13:01:57 +0200

    Servicing the new driverresult service

---
+++ FormuleCirkelEntity/Services/IDriverResultService.cs
@@ -0,0 +1,27 @@
+﻿using FormuleCirkelEntity.DAL;
+using FormuleCirkelEntity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormuleCirkelEntity.Services
+{
+    public interface IDriverResultService : IDataService<DriverResult>
+    {
+        Task<List<DriverResult>> GetAllResultsFromDriver(int driverId);
+    }
+
+    public class DriverResultService : DataService<DriverResult>, IDriverResultService
+    {
+        public DriverResultService(FormulaContext context) : base(context) { }
+
+        public async Task<List<DriverResult>> GetAllResultsFromDriver(int driverId)
+        {
+            return await Data.AsNoTracking()
+                .Where(res => res.SeasonDriver.DriverId == driverId)
+                .ToListAsync();
+        }
+    }
+}
